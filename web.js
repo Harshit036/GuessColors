ce829124f4bbcc6b2976957b3adcffafbfd12d7e
Harshit036 <harshit_v@ce.iitr.ac.in>
2021-08-21 11:18:27 +0530

    First Commit

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-function makeRandomColor() { 
+function makeRandomColor() {
     return Math.round(Math.random() * 255);
 }
 
@@ -17,19 +17,30 @@ function giveColor() {
         green = makeRandomColor();
         blue = makeRandomColor();
 
+        if (i == guess) {
+            document.getElementById("color").innerHTML = `${red},${green},${blue}`;
+        }
         document.getElementById(`item${i}`).style.backgroundColor = `rgb(${red},${green},${blue})`;
         document.getElementById(`item${i}`).addEventListener("click", () => {
 
             if (document.getElementById(`item${guess}`) == document.getElementById(`item${i}`)) {
-                document.getElementById('result').innerHTML = "Right Answer";
+                document.getElementById('result').innerHTML = "Congratulations! You got the right answer";
+                document.getElementById('result').style.color = "rgb(5, 136, 49)";
             }
             else {
-                document.getElementById('result').innerHTML = "Wrong Answer";
+                document.getElementById('result').innerHTML = "Wrong! Try Again :(";
+                document.getElementById('result').style.color = "red";
             }
 
-            if (i == guess) {
-                document.getElementById("color").innerHTML = `${red},${green},${blue}`;
-            }
         });
+    }
+}
+
+function reset() {
+    document.getElementById('color').innerHTML = "";
+    document.getElementById('result').innerHTML = "";
+    for (let i = 1; i <= 6; i++) {
+        document.getElementById(`item${i}`).style.backgroundColor = "rgb(43, 226, 104)";
+
     }
 }
